5b0b3b7fcc38a0c29c041af68ebd7497d2e0bc20
miguelsndc <mglsndc@gmail.com>
2022-04-27 22:34:20 -0300

    feat: stack

---
+++ src/Stack.java
@@ -0,0 +1,35 @@
+public class Stack <T> implements Iterable<T> {
+    private java.util.LinkedList<T> list = new java.util.LinkedList<T>();
+
+    public Stack () {}
+
+    public Stack (T firstElement) {
+        push(firstElement);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty () {
+        return size() == 0;
+    }
+
+    public void push(T element) {
+        list.addLast(element);
+    }
+
+    public T pop() {
+        if(isEmpty()) throw new java.util.EmptyStackException();
+        return list.removeLast();  
+    }
+
+    public T peek() {
+        if(isEmpty()) throw new java.util.EmptyStackException();
+        return list.peekLast();
+    }
+
+    @Override public java.util.Iterator <T> iterator() {
+        return list.iterator();
+    }
+}
